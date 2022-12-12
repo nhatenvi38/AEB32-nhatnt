d3cd4e74effaa0ecf0d8d2fb32d71890f59fd292
nhatenvi38 <nhatenvi38@gmail.com>
2022-12-12 21:13:57 +0700

    practice

--- practice/Session 08 JS/bt4.js
+++ practice/Session 08 JS/bt4.js
@@ -1,7 +1,9 @@
-let n = prompt ('nhap so nguyen duong co hai chu so n');
-let res1 = ((n/10)% 10);{
-
-
-console.log (res1)};
-let res2 = (n % 10);{
-console.log (res2)}
+let n = prompt("nhap so nguyen duong co hai chu so n");
+let res1 = (((n / 10) % 10)*10);
+{
+  console.log(res1);
+}
+let res2 = n % 10;
+{
+  console.log(res2);
+}
