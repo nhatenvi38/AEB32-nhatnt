188b94665cf7129312f09ff90427b91d04ac43c4
nhatenvi38 <nhatenvi38@gmail.com>
2022-12-14 15:08:04 +0700

    practice

---
+++ practice/Session 08 JS/bt6.js
@@ -0,0 +1,9 @@
+let n = prompt("nhap so nguyen duong n");
+let m = prompt("nhap so nguyen duong m");
+let p;
+for (let x = 0; x <= m && x <= n; x++) {
+  if (m % x === 0 && n % x === 0) {
+    p = x;
+  }
+}
+console.log(p);
